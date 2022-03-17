0f38d5ea282a31df7dcc7e62e0dc7b4cc9a35888
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-17 15:21:37 +0900

    space -> tab

--- src/ArgumentBuilder.ts
+++ src/ArgumentBuilder.ts
@@ -1,36 +1,36 @@
 export default class ArgumentBuilder
 {
-    #args: string[] = []
+	#args: string[] = []
 
-    Append(arg: string): void
-    Append(arg: string, param: string): void
-    Append(args: string[]): void
+	Append(arg: string): void
+	Append(arg: string, param: string): void
+	Append(args: string[]): void
 
-    Append(arg: string | string[], param?: string): void
-    {
-        if (Array.isArray(arg)) {
-            this.#args = this.#args.concat(arg)
-        } else {
-            this.#args.push(arg)
+	Append(arg: string | string[], param?: string): void
+	{
+		if (Array.isArray(arg)) {
+			this.#args = this.#args.concat(arg)
+		} else {
+			this.#args.push(arg)
 
-            if (param != null) {
-                this.#args.push(arg)
-            }
-        }
-    }
+			if (param != null) {
+				this.#args.push(arg)
+			}
+		}
+	}
 
-    Count(): number
-    {
-        return this.#args.length
-    }
+	Count(): number
+	{
+		return this.#args.length
+	}
 
-    Build(): string[]
-    {
-        return this.#args
-    }
+	Build(): string[]
+	{
+		return this.#args
+	}
 
-    ToString(): string
-    {
-        return this.#args.join(' ')
-    }
+	ToString(): string
+	{
+		return this.#args.join(' ')
+	}
 }
